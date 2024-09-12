ef306db1518deae1e8684b8b56d91f09c5a83214
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-09-12 22:55:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruit_data(fruit):
     else:
         return None
         
-my_dataframe = session.table("smoothies.public.fruit_options")#.select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data = my_dataframe, use_container_width=True)
 #st.stop()
 pd_df = my_dataframe.to_pandas()
