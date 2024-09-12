91a87733e7dc9ab158d4f2fe328de4a03dbe808c
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-09-12 17:59:02 +0530

    Update streamlit_app.py
    make session as cmnt

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smothie will be: ",name_on_order)
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data = my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect("Choose up to 5 ingredients:",my_dataframe, max_selections =5)
@@ -32,7 +32,7 @@ if ingredients_list:
     #st.stop()
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        #session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 
