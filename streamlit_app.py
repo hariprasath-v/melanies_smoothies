1c8684ea79c89d291dfbe6edd1963b15ba9c2509
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-09-12 22:59:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_dataframe = session.table("smoothies.public.fruit_options")#.select(col('FRUI
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
-ingredients_list = st.multiselect("Choose up to 5 ingredients:",my_dataframe, max_selections =5)
+ingredients_list = st.multiselect("Choose up to 5 ingredients:",pd_df['FRUIT_NAME'].unique(), max_selections =5)
 if ingredients_list:
     ingredients_string = ""
     for fruit_choosen in ingredients_list:
