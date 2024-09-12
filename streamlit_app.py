ac9e117ca33c8514c65e0a88c0e79af6197cc6d4
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-09-12 18:02:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smothie will be: ",name_on_order)
-
+"""
 #session = get_active_session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data = my_dataframe, use_container_width=True)
@@ -34,6 +34,5 @@ if ingredients_list:
     if time_to_insert:
         #session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-
+"""
 
